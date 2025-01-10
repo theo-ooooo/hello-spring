a5c8d6e417a7acb8f5b304da70bf9dedf46185cd
Theo <73323028+kkwondev@users.noreply.github.com>
2025-01-10 07:24:47 +0000

    init

---
+++ src/main/java/hello/hello_spring/controller/helloController.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class helloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "h11ello!113");
+     return "hello";
+    }
+}
