6c0e24336644f4ce6eb1d6b4e8e7a83b4485be8a
Theo <73323028+kkwondev@users.noreply.github.com>
2025-01-13 02:37:51 +0000

    세션4 : 회원 서비스 개발, 회원 서비스 테스트 추가로 내가 만들어봄 단일_유저_검색, 전체_유저_리스트

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -2,11 +2,12 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -50,18 +51,44 @@ class MemberServiceTest {
         // when
         memberService.join(member1);
 
+
+        // then
+
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+    @Test
+    void 전체_유저_리스트() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        Member member2 = new Member();
+        member2.setName("spring2");
+        // when
+        memberService.join(member1);
+        memberService.join(member2);
+
         // then
-
+        List<Member> members = memberService.findMembers();
+        assertThat(members.size()).isEqualTo(2);
     }
 
     @Test
-    void findMembers() {
-    }
+    void 단일_유저_검색() {
 
-    @Test
-    void findMember() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring4444");
+        // when
+        long setId = memberService.join(member1);
+
+        // then
+        Optional<Member> member = memberService.findMember(setId);
+
+        member.ifPresentOrElse(m -> assertThat(m.getName()).isEqualTo(member1.getName()), Assertions::fail);
+
     }
 }
