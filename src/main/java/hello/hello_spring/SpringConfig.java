ba5033e68026553625a1ba206197ac4139a92470
Theo <73323028+kkwondev@users.noreply.github.com>
2025-01-13 07:54:15 +0000

    세션7 : 스프링 DB 접근 기술 스프링 데이타 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,12 +1,11 @@
 package hello.hello_spring;
-import hello.hello_spring.repository.JdbcMemberRepository;
-import hello.hello_spring.repository.JdbcTemplateMemberRepository;
-import hello.hello_spring.repository.JpaMemberRepository;
+//import hello.hello_spring.repository.JdbcMemberRepository;
+//import hello.hello_spring.repository.JdbcTemplateMemberRepository;
+//import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
-//import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,28 +14,22 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-//   private final DataSource dataSource;
-//
-//   @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
+    private final MemberRepository  memberRepository;
 
-    private EntityManager em;
-
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
